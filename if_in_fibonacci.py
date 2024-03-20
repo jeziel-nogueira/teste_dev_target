71159cb1c1912106655284c74d9007ca47eb4644
jeziel nogueira <jezielnogueira_15@yahoo.com.br>
2024-03-20 05:34:03 -0300

    add main()

--- if_in_fibonacci.py
+++ if_in_fibonacci.py
@@ -1,25 +1,36 @@
 import random
 
-value_b = 1
 
-sum, value_A, value_to_check = 0, 0, 0
+def check_number_exist(num):    
+    value_b = 1
+    value_A, sum = 0, 0
 
-check_input = input("Se deseja informar um valor digite 1;\n ou pressione enter para um número aleatório: ")
-if check_input == '1':
-    value_to_check = int(input("informe o valor: "))
-else:
-    value_to_check = random.randint(0, 100)
-    print(f'Valor randômico: ', value_to_check)
+    while sum <= num:
+        sum = value_A + value_b
+        if sum == num:
+            break
+        value_A = value_b
+        value_b = sum
 
-while sum <= value_to_check:
-    sum = value_A + value_b
-    if sum == value_to_check:
-        break
-    value_A = value_b
-    value_b = sum
+    if sum == num:
+        print(f'O valor {num} pertence a sequência de Fibonacci')
+    else:
+        print(f'O valor {num} não pertence a sequência de Fibonacci')
 
 
-if sum == value_to_check:
-    print(f'O valor {value_to_check} pertence a sequência de Fibonacci')
-else:
-    print(f'O valor {value_to_check} não pertence a sequência de Fibonacci')
+def main():
+    while 1:
+        check_input = input(" * Se deseja informar um valor digite 1;\n * Pressione enter para um número aleatório \n * Ou informe 2 para finalizar: \n")
+        if check_input == '1':
+            check_number_exist(int(input("informe o valor: ")))
+        elif check_input == '2':
+            break
+        else:
+            value_to_check = random.randint(0, 100)
+            print(f'Valor randômico: ', value_to_check)
+            check_number_exist(value_to_check)
+        
+
+if __name__ == "__main__":
+    main()
+
